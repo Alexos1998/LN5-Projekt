58d9116e64aaa5f65e043ab9de166313ef6d75b3
Alexander Jeddeloh <alex.jeddeloh@gmail.com>
2021-06-13 21:21:49 +0200

    Menü mit 2 Seiten und Highlights

--- projekt.py
+++ projekt.py
@@ -1,7 +1,7 @@
 import random
 import time
 from tkinter import *
-from entities import *
+from tkinter.font import Font
 
 def setup():
     global spielfeld, canvasSpielfeld, canvasTextfeld, img
@@ -39,38 +39,46 @@ def intro():
 
 def buttonErsteRunde():
     global button1
+
     button1 = Button(spielfeld, text="Auf geht's", highlightthickness=0, bd=0, bg="white",fg="#333", command=lambda: ersteRunde())
     canvasTextfeld.create_window(400, 280, window=button1)
     spielfeld.bind("<KeyPress-Return>", lambda b: ersteRunde())
 
 def figuren():
     global canvasSpielfeld, spielerFigur, figurAnzeige
+
     spielerFigur = PhotoImage(file="Assets/Gruppe.png")
     figurAnzeige = canvasSpielfeld.create_image(20, 220, anchor=NW, image=spielerFigur, tags="figure")
 
 
 def ersteRunde():
-    global canvasTextfeld, button1, delayFrage, menuselection
+    global canvasTextfeld, button1, delayFrage, menuSelectionUpDown
+
     canvasTextfeld.delete(introText)
     button1.destroy()
-    #canvasSpielfeld.delete("figure")
-    frage()
-    spielfeld.after(delayFrage, lambda: menu())
+    spielfeld.after(100, lambda: menu())
     menuNavigation()
 
 def menuNavigation():
+    global menuSelectionUpDown
 
-    spielfeld.bind("<KeyPress-Down>", lambda b:test(b))
-    spielfeld.bind("<KeyPress-Up>", lambda b: test(b))
-    spielfeld.bind("<KeyPress-Right>", lambda b: test(b))
-    spielfeld.bind("<KeyPress-Left>", lambda b: test(b))
+    spielfeld.bind("<KeyPress-Down>", lambda b: menuHighlight(b))
+    spielfeld.bind("<KeyPress-Up>", lambda b: menuHighlight(b))
+
+    spielfeld.bind("<KeyPress-Right>", lambda b: menuHighlight(b))
+    spielfeld.bind("<KeyPress-Left>", lambda b: menuHighlight(b))
+
     spielfeld.bind("<KeyPress-Return>", lambda b: print("geht"))
 
 def menu():
-    global menuButton1, menuButton2, menuButton3, canvasTextfeld, spielfeld, menuselection
+    global menuButton1, menuButton2, menuButton3, canvasTextfeld, spielfeld, menuListMoves, menuListSpecial
+
+    menuListMoves = canvasTextfeld.create_text(10,5, width= 650, anchor = NW, text="Moves", fill="#000", font=("Press Start 2P", 35))
+    menuListSpecial = canvasTextfeld.create_text(280,5, width= 650, anchor = NW, text="Spezial", fill="#333", font=("Press Start 2P", 30))
+
 
     menuButton1 = Button(spielfeld, text="Power Nap", bg="white",
-                         highlightthickness=0, bd=0, fg="#333",
+                         highlightthickness=0, bd=0, fg="#000",
                          font=("Press Start 2P", 25),
                          command=lambda: print("Funktioniert super"))
     canvasTextfeld.create_window(100, 100, window=menuButton1, anchor=W)
@@ -81,45 +89,62 @@ def menu():
                          command=lambda: print("Funktioniert super"))
     canvasTextfeld.create_window(100, 170, window=menuButton2, anchor=W)
 
-    menuButton3 = Button(spielfeld, text="Kaffe Booster", bg="white",
+    menuButton3 = Button(spielfeld, text="Kaffee Booster", bg="white",
                          highlightthickness=0, bd=0, fg="#333",
                          font=("Press Start 2P", 20),
                          command=lambda: print("Funktioniert super"))
     canvasTextfeld.create_window(100, 240, window=menuButton3, anchor=W)
 
+#def menuLeftRight(keystroke):
 
-def test(keystroke):
-    global menuButton1, menuButton2, menuButton3, menuselection
+
+def menuHighlight(keystroke):
+
+    global menuSelectionUpDown, menuButton1, menuButton2, menuButton3, menuSelectionLeftRight, canvasTextfeld, menuListMoves, menuListSpecial
+
+
+    if keystroke.keysym =="Right":
+        menuSelectionUpDown = 0
+        if menuSelectionLeftRight == 0:
+            menuSelectionLeftRight += 1
+    elif keystroke.keysym =="Left":
+        menuSelectionUpDown = 0
+        if menuSelectionLeftRight == 1:
+            menuSelectionLeftRight -= 1
+
+    if menuSelectionLeftRight < 1:
+        canvasTextfeld.itemconfigure(menuListMoves, fill="#000", font=("Press Start 2P",35))
+        canvasTextfeld.itemconfigure(menuListSpecial, fill="#333", font=("Press Start 2P", 30))
+        menuButton1.configure(text="Power Nap")
+        menuButton2.configure(text="Energy Booster")
+        menuButton3.configure(text="Kaffee Booster")
+    elif menuSelectionLeftRight == 1:
+        canvasTextfeld.itemconfigure(menuListMoves, fill="#333", font=("Press Start 2P", 30))
+        canvasTextfeld.itemconfigure(menuListSpecial, fill="#000", font=("Press Start 2P", 35))
+        menuButton1.configure(text="Gruppenarbeit")
+        menuButton2.configure(text="Nachtschicht")
+        menuButton3.configure(text="Keine Idee mehr")
+
     if keystroke.keysym == "Down":
-        if menuselection < 2:
-            menuselection += 1
+        if menuSelectionUpDown < 2:
+            menuSelectionUpDown += 1
     elif keystroke.keysym == "Up":
-        if menuselection > 0:
-            menuselection -= 1
+        if menuSelectionUpDown > 0:
+            menuSelectionUpDown -= 1
 
-    if menuselection < 1:
-        menuButton1.configure(font=("Press Start 2P",25))
-        menuButton2.configure(font=("Press Start 2P",20))
-        menuButton3.configure(font=("Press Start 2P",20))
-    elif menuselection == 1:
-        menuButton2.configure(font=("Press Start 2P",25))
-        menuButton1.configure(font=("Press Start 2P",20))
-        menuButton3.configure(font=("Press Start 2P",20))
-    elif menuselection > 1:
-        menuButton3.configure(font=("Press Start 2P",25))
-        menuButton1.configure(font=("Press Start 2P",20))
-        menuButton2.configure(font=("Press Start 2P",20))
+    if menuSelectionUpDown == 0:
+        menuButton1.configure(fg="black", font=("Press Start 2P",25))
+        menuButton2.configure(fg="#333",font=("Press Start 2P",20))
+        menuButton3.configure(fg="#333",font=("Press Start 2P",20))
+    elif menuSelectionUpDown == 1:
+        menuButton2.configure(fg="black", font=("Press Start 2P",25))
+        menuButton1.configure(fg="#333",font=("Press Start 2P",20))
+        menuButton3.configure(fg="#333",font=("Press Start 2P",20))
+    elif menuSelectionUpDown == 2:
+        menuButton3.configure(fg="black",font=("Press Start 2P",25))
+        menuButton2.configure(fg="#333",font=("Press Start 2P",20))
+        menuButton1.configure(fg="#333", font=("Press Start 2P", 20))
 
-def frage():
-    global canvasTextfeld, delayFrage
-    frageSpielbeginn = "Moves "
-    frageSpielbeginnText = canvasTextfeld.create_text(10,5, width= 650, anchor = NW, text="", fill="#333", font=("Press Start 2P", 30))
-
-    for x in range(len(frageSpielbeginn)):
-        delayFrage = 40 * x
-        frage = frageSpielbeginn[:x]
-        frageUpdate = lambda frage=frage: canvasTextfeld.itemconfigure(frageSpielbeginnText, text=frage)
-        canvasTextfeld.after(delayFrage, frageUpdate)
 
 
 #Erstellen des Tkinter Fensters. Hintergrund wird auf schwarz gesetzt
@@ -128,7 +153,8 @@ spielfeld.title("Pokemon in gut")
 spielfeld.geometry("800x800")
 spielfeld.configure(bg="#333")
 
-menuselection = 0
+menuSelectionUpDown = 0
+menuSelectionLeftRight = 0
 
 setup()
 intro()
