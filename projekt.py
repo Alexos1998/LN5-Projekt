3814489ae3109fa92feb1ebfa340977c4abca064
Alexander Jeddeloh <alex.jeddeloh@gmail.com>
2021-06-16 00:33:43 +0200

    Healthbar angepasst. Farben und Prozent-Anzeige implementiert

--- projekt.py
+++ projekt.py
@@ -21,18 +21,19 @@ def setup():
     projektFigur = PhotoImage(file="Assets/coronavirus.png")
     projektFigurAnzeige = canvasSpielfeld.create_image(0, 150, anchor=E, image=projektFigur)
 
+
+
     figuren(800, 0)
 
 def figuren(frames, durchlaeufe):
-    global spielerFigurAnzeige, projektFigurAnzeige
+    global spielerFigurAnzeige, projektFigurAnzeige, currentKP
+
 
     # movespielerFigur = lambda: canvasSpielfeld.move(spielerFigurAnzeige,-5,0);frames -= 5
     # canvasSpielfeld.after(frames*2, movespielerFigur)
     #
     # moveprojektFigur = lambda: canvasSpielfeld.move(projektFigurAnzeige,+5,0)
     # canvasSpielfeld.after(frames*2, moveprojektFigur)
-    # print(frames)
-
 
     movespielerFigur = lambda: canvasSpielfeld.move(spielerFigurAnzeige,-5,0);frames -= 5
     canvasSpielfeld.after(durchlaeufe*10, movespielerFigur)
@@ -40,43 +41,47 @@ def figuren(frames, durchlaeufe):
     moveprojektFigur = lambda: canvasSpielfeld.move(projektFigurAnzeige,+5,0)
     canvasSpielfeld.after(durchlaeufe*10, moveprojektFigur)
 
-
-
     if frames == 20:
         canvasSpielfeld.after(1700, lambda: intro())
         canvasSpielfeld.after(1700, lambda: healthBar(1,1))
+
     else:
         durchlaeufe +=1
         figuren(frames, durchlaeufe)
 
 def healthBar(spieler,projekt):
+    global currentKP, spielerArrow
+
+    spielerArrow = PhotoImage(file="Assets/arrowSpieler.png")
+    spielerHealthBarArrow = canvasSpielfeld.create_image(450, 425, anchor=W, image=spielerArrow)
 
 
+
+
+    currentKPIndex = str(100*spieler) +"/ 100"
     currentHealthProjekt = 200 * projekt + 123
     currentHealthSpieler = 200 * spieler + 500
 
+    lifeBarColorSpieler = "green" if spieler > 0.8 else "orange" if spieler < 0.8 and spieler > 0.5 else "yellow" if spieler < 0.5 and spieler > 0.2 else "red"
+    lifeBarColorProjekt = "green" if projekt > 0.8 else "orange" if projekt < 0.8 and projekt > 0.5 else "yellow" if projekt < 0.5 and projekt > 0.2 else "red"
+
     healthbarProjektWhite = canvasSpielfeld.create_rectangle(123, 100, 323, 120, fill="white")
-    healthbarProjektWhite = canvasSpielfeld.create_rectangle(123, 100, currentHealthProjekt, 120, fill="green")
+    healthbarProjektColor = canvasSpielfeld.create_rectangle(123, 100, currentHealthProjekt, 120, fill=lifeBarColorProjekt)
 
     healthbarSpielerWhite = canvasSpielfeld.create_rectangle(700, 400, 500, 380, fill="white")
-    healthbarSpielerWhite = canvasSpielfeld.create_rectangle(currentHealthSpieler, 400, 500, 380, fill="green")
+    healthbarSpielerColor = canvasSpielfeld.create_rectangle(currentHealthSpieler, 400, 500, 380, fill=lifeBarColorSpieler)
 
     kraftpunkte = canvasSpielfeld.create_text(100,112,text="KP:", font=("Press Start 2P", 15), tags="KP")
     kraftpunkte = canvasSpielfeld.create_text(477, 392, text="KP:", font=("Press Start 2P", 15), tags="KP")
-    currentKP = canvasSpielfeld.create_text(600, 440, text= str(100*spieler) +"/ 100", anchor= S, font=("Press Start 2P", 18), tags="KP")
-
-    arrowSpieler = PhotoImage(file="Assets/Gruppe.png")
-    decorationSpieler= canvasTextfeld.create_image(100,100, anchor=E, image=arrowSpieler)
+    if spieler == 1:
+        currentKP = canvasSpielfeld.create_text(600, 440, text= currentKPIndex, anchor= S, font=("Press Start 2P", 18))
+    else:
+        canvasSpielfeld.itemconfigure(currentKP, text=currentKPIndex)
 
 def intro():
     global introText, canvasTextfeld, canvasSpielfeld
 
-
-
     introText = canvasTextfeld.create_text(10,5, width=750, anchor=NW, text="", font=("Press Start 2P", 35))
-
-
-
 
     intro1 = "Ein wildes PROJEKT ist aufgetaucht! "
     for x in range(len(intro1)):
@@ -84,13 +89,6 @@ def intro():
         text = intro1[:x]
         textUpdate = lambda text=text: canvasTextfeld.itemconfigure(introText, text=text)
         canvasTextfeld.after(delay, textUpdate)
-
-    # intro2= "Du hast nur noch 10 Stunden Zeit! "
-    # for x in range(len(intro2)):
-    #     delay2 = delay + 1500 + 40*x
-    #     text = intro2[:x]
-    #     textUpdate = lambda text=text: canvasTextfeld.itemconfigure(introText, text=text)
-    #     canvasTextfeld.after(delay2, textUpdate)
 
     spielfeld.after(delay + 500, lambda: buttonErsteRunde())
 
@@ -109,39 +107,36 @@ def ersteRunde():
     spielfeld.after(100, menu())
 
 def menu():
-    global menuButton1, menuButton2, menuButton3, canvasTextfeld, spielfeld, menuListMoves, menuListSpecial
+    global menuButton1, menuButton2, menuButton3, spielfeld, selectionMarker, selection, menuListMoves, menuListSpecial
 
-    # menuListMoves = canvasTextfeld.create_text(10,5, width= 650, anchor = NW, text="Moves", fill="#000", font=("Press Start 2P", 35))
-    # menuListSpecial = canvasTextfeld.create_text(280,5, width= 650, anchor = NW, text="Spezial", fill="#555", font=("Press Start 2P", 30))
     menuListMoves = Button(spielfeld, text="Moves", bg="#fff",
                          highlightthickness=0, bd=0, fg="#000",
-                         font=("Press Start 2P", 30),
-                         command= lambda: Move1())
+                         font=("Press Start 2P", 30))
     canvasTextfeld.create_window(10, 0, window=menuListMoves, anchor=NW)
 
     menuListSpecial = Button(spielfeld, text="Spezial", bg="#fff",
                          highlightthickness=0, bd=0, fg="#555",
-                         font=("Press Start 2P", 30),
-                         command=lambda: Move1())
+                         font=("Press Start 2P", 30))
     canvasTextfeld.create_window(280, 0, window=menuListSpecial, anchor=NW)
 
     menuButton1 = Button(spielfeld, text="Power Nap", bg="#fff",
-                         highlightthickness=0, bd=0, fg="#555",
-                         font=("Press Start 2P", 20),
-                         command=lambda: Move1())
+                         highlightthickness=0, bd=0, fg="#555", anchor=W,
+                         font=("Press Start 2P", 20))
     canvasTextfeld.create_window(100, 100, window=menuButton1, anchor=W)
 
     menuButton2 = Button(spielfeld, text="Energy Booster", bg="#fff",
-                         highlightthickness=0, bd=0, fg="#555",
-                         font=("Press Start 2P", 20),
-                         command=lambda: Move2())
+                         highlightthickness=0, bd=0, fg="#555", anchor=W,
+                         font=("Press Start 2P", 20))
     canvasTextfeld.create_window(100, 170, window=menuButton2, anchor=W)
 
     menuButton3 = Button(spielfeld, text="Kaffee Booster", bg="#fff",
-                         highlightthickness=0, bd=0, fg="#555",
-                         font=("Press Start 2P", 20),
-                         command=lambda: Move3())
+                         highlightthickness=0, bd=0, fg="#555", anchor=W,
+                         font=("Press Start 2P", 20))
     canvasTextfeld.create_window(100, 240, window=menuButton3, anchor=W)
+
+    # selection = PhotoImage(file="Assets/selection.png")
+    # selectionMarker = canvasTextfeld.create_image(100, 100, anchor=E, image=selection)
+
     menuNavigation()
 
 def menuNavigation():
@@ -151,11 +146,14 @@ def menuNavigation():
     spielfeld.bind("<KeyPress-Up>", lambda b: menuHighlight(b))
     spielfeld.bind("<KeyPress-Right>", lambda b: menuHighlight(b))
     spielfeld.bind("<KeyPress-Left>", lambda b: menuHighlight(b))
-    spielfeld.bind("<KeyPress-Return>", lambda b: action())
+    spielfeld.bind("<KeyPress-Return>", lambda b: canvasSpielfeld.delete("all"))
 
 def menuHighlight(keystroke):
 
-    global  menuButton1, menuButton2, menuButton3, menuSelectionLeftRight, menuSelectionUpDown, menuListMoves, menuListSpecial
+    global menuSelectionUpDown, menuSelectionLeftRight
+
+    # moveMarkerDown = lambda: canvasTextfeld.move(selectionMarker, 0, -70)
+    # moveMarkerUp = lambda: canvasTextfeld.move(selectionMarker, 0,70)
 
 
     if keystroke.keysym =="Right" or keystroke == "Right":
@@ -168,23 +166,25 @@ def menuHighlight(keystroke):
             menuSelectionLeftRight -= 1
 
     if menuSelectionLeftRight < 1:
-        menuListMoves.configure(fg="#000")#, font=("Press Start 2P",35))
-        menuListSpecial.configure(fg="#555")#, font=("Press Start 2P", 30))
+        menuListMoves.configure(fg="#000")
+        menuListSpecial.configure(fg="#555")
         menuButton1.configure(text="Power Nap")
         menuButton2.configure(text="Energy Booster")
         menuButton3.configure(text="Kaffee Booster")
     elif menuSelectionLeftRight == 1:
-        menuListMoves.configure(fg="#555")#, font=("Press Start 2P", 30))
-        menuListSpecial.configure(fg="#000")#, font=("Press Start 2P", 35))
+        menuListMoves.configure(fg="#555")
+        menuListSpecial.configure(fg="#000")
         menuButton1.configure(text="Gruppenarbeit")
         menuButton2.configure(text="Nachtschicht")
         menuButton3.configure(text="Keine Idee mehr")
 
     if keystroke.keysym == "Down":
         if menuSelectionUpDown < 3:
+            # canvasTextfeld.after(0, moveMarkerDown)
             menuSelectionUpDown += 1
     elif keystroke.keysym == "Up":
         if menuSelectionUpDown > 1:
+            # canvasTextfeld.after(0, moveMarkerUp)
             menuSelectionUpDown -= 1
 
     if menuSelectionUpDown == 1:
@@ -208,7 +208,7 @@ def menuHighlight(keystroke):
 def action():
     te
 
-#Erstellen des Tkinter Fensters. Hintergrund wird auf schwarz gesetzt
+#Erstellen des Tkinter Fensters. Hintergrund wird auf dunkel grau gesetzt
 spielfeld = Tk()
 spielfeld.title("Pokemon in gut")
 spielfeld.geometry("800x800")
